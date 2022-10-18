86bd135f2a70d464c2ccf4d74ab45824c109099b
JIMSHADTP <108132159+JIMSHADTP@users.noreply.github.com>
2022-10-18 18:34:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css'
-import { useState } from 'react'
+import {useState} from 'react'
 function App() {
   const [toDos, setTodos] = useState([])
   const [toDo, setTodo] = useState('')
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -14,25 +15,31 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setTodos([...toDos,{id:Date.now(), Text:toDo,status:false}])} className="fas fa-plus"></i>
+        <i onClick={() => setTodos([...toDos, { id: Date.now(), Text: toDo, status: false }])} className="fas fa-plus"></i>
+
       </div>
       <div className="todos">
         {
           toDos.map((obj) => {
-return (
-            <div className="todo">
-              <div className="left">
-                <input onChange={(e)=> {
-                  console.log(e.target.value)
-                  console.log(obj)
-                  setTodos(toDos.filter(obj))
-                }} value={obj.status} type="checkbox" name="" id="" />
-                <p>{obj.Text}</p>
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input onChange={(e) => {
+                    console.log(e.target.checked)
+                    console.log(obj)
+                    setTodos(toDos.filter(obj2 => {
+                      if (obj2.id === obj.id) {
+                        obj2.status = e.target.checked
+                      }
+                      return obj2
+                    }))
+                  }} value={obj.status} type="checkbox" name="" id="" />
+                  <p>{obj.Text}</p>
+                </div>
+                <div className="right">
+                  <i className="fas fa-times"></i>
+                </div>
               </div>
-              <div className="right">
-                <i className="fas fa-times"></i>
-              </div>
-            </div>
             )
           })
         }
