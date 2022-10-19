87919756922cda959652538f2429421f73146c4a
JIMSHADTP <108132159+JIMSHADTP@users.noreply.github.com>
2022-10-19 15:03:37 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
     return (initialValue || "")
   });
 
-  // const index = toDos && toDos.findIndex(obj => obj.statusRemove == true);
-  // if (index > -1) toDos && toDos.splice((index), 1);
 
   const dayNames = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
   const date = new Date()
@@ -51,17 +49,17 @@ function App() {
         </div>
         <div className="subHeading">
           <br />
-          <h2 className='gradient-text2'>Hi, it's {day}</h2>
+          <h2 className='gradient-text2'>Good Morning, it's {day}</h2>
         </div>
       </div>
 
       <form onSubmit={handleInputSubmit} >
         <div className="toDoInput">
           <div className="left">
-            <input value={toDo} onChange={handleUserInput} type="text" placeholder="Plan something...." />
+            <input value={toDo} onChange={handleUserInput} autoFocus type="text" placeholder="Plan something...." />
           </div>
           <div className="right erase">
-            <i onClick={resetInputField} className="fas fa-eraser" title='clear'></i>
+            <i onClick={resetInputField}  className="fas fa-eraser" title='clear'></i>
           </div>
           <div className="rightEnd  add">
             <button style={{ border: 'none', outline: 'none', backgroundColor: '#fff' }} type="submit"><i className="fas fa-plus" title="Add"></i></button>
@@ -97,7 +95,7 @@ function App() {
       </div>
 
       <div className="container onGoing">
-        <h2>on Going</h2>
+        <h2>On Going</h2>
         {
           toDos && toDos.map((obj) => {
             if (!obj.statusDone && !obj.statusDrop) {
